4cb1a436381b81cc7bfb398010f831a08f032ca1
shrutipanchal <38175306+shrutipanchal@users.noreply.github.com>
2020-12-02 16:41:06 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// to load the modules
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+//init app
+const app = express();
+
+//load files
+const keys = require('./config/keys');
+
+
+//setup view engine
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine','handlebars');
+
+//connect client side to server css and js file
+app.use(express.static('public'));
+
+//create port
+const port = process.env.PORT || 3000;
+
+
+//handle home route
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'About Page'
+    });
+});
+
+app.get('/contact',(req,res)=>{
+    res.render('contact',{
+        title: 'Contact Us'
+    });
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('SignUpForm',{
+        title: 'Register'
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+;    //console.log('Server is up on port'+ port);
+});
