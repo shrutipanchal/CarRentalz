00770b0fadcf7e494a443db986e20b26219dd0df
shrutipanchal <38175306+shrutipanchal@users.noreply.github.com>
2020-12-03 01:10:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 // to load the modules
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
 //init app
 const app = express();
 
+//setup body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//configuration for authentication
+app.use(cookieParser());
+app.use(session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 //load files
 const keys = require('./config/keys');
 
+//load collections
+const User = require('./models/user');
+
+//connect to MongoDB
+mongoose.connect(keys.MongoDB,()=> {
+        console.log('MongoDB is connected ...');
+    }).catch((err)=>{
+    console.log(err);
+});
 
 //setup view engine
 app.engine('handlebars',exphbs({
@@ -21,6 +51,8 @@ app.use(express.static('public'));
 //create port
 const port = process.env.PORT || 3000;
 
+
+const Contact = require('./models/contact');
 
 //handle home route
 app.get('/',(req,res)=>{
@@ -37,6 +69,8 @@ app.get('/cars',(req,res)=>{
     res.render('cars',{
         title: 'Cars'
     });
+
+
 });
 
 app.get('/contact',(req,res)=>{
@@ -44,6 +78,24 @@ app.get('/contact',(req,res)=>{
         title: 'Contact Us'
     });
 });
+//save contact form data
+app.post('/contact',(req,res) =>{
+    console.log(req.body);
+    const newContact = {
+        email: req.body.email,
+        name: req.body.name,
+        message: req.body.message
+    }
+    new Contact(newContact).save((err,user)=>{
+        //  if(err){
+        //      throw err;
+        //  }
+        //  else{
+            console.log('Message was received', user);
+         //}
+    });
+});
+
 
 app.get('/signup',(req,res)=>{
     res.render('SignUpForm',{
